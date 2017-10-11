b793ee0aac5d3fc3067bc99bf3c528ebb31f672a
Phanindra Veera <phanindrasrikar@gmail.com>
2017-10-11 14:30:44 -0400

    Link top title

--- app/components/header.js
+++ app/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = (props) => {
   return (
     <div>
-      <h1>Quaker -- All hail the Earthquakes</h1>
+      <a href="/"><h1>Quaker -- All hail the Earthquakes</h1></a>
       <p style={{'textAlign':'center'}}>Click on either one of the buttons below to get information about
         earthquakes happening around the world..!</p>
     </div>
